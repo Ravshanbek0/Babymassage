352ddbf633eb477462ea8cacd5ecb37a63d341b1
Ravshanbek0 <rnabijonov66@gmail.com>
2024-12-09 14:00:22 +0500

    Join button's bg color has changed

--- src/pages/registration/Register.jsx
+++ src/pages/registration/Register.jsx
@@ -77,7 +77,7 @@ function Register() {
                         <div>
                             <button
                                 type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="flex w-full text-custom-white justify-center rounded-md  px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-custom-blue"
                             >
                                 Jo'natish
                             </button>
